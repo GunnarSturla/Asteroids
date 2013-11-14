5bdef8111f06f51b3c68d2cb71d5bbe9805d6f68
GunnarSturla <gunnar@gunnarsturla.com>
2013-11-14 14:44:03 +0000

    Penis

--- SpaceObject.java
+++ SpaceObject.java
@@ -1,36 +1,35 @@
 public abstract class SpaceObject {
 	public double x, y;
-	public final int rotation;
-	public final double direction, speed;
+	public int rotation, direction;
+	public double speed;
 	
 	
 	// Notkun:	a = new SpaceObject(x, y, 0.01, 90, 0);
 	// 
-	public static void SpaceObject(double x, double y, double spd, double dir, int rot) {
+	public void SpaceObject(double x, double y, double spd, int dir, int rot) {
 		this.x = x;
 		this.y = y;
-		speed = spd;
-		direction = dir % 360;
-		rotation = rot % 360;
+		speed = 0.1;
+		this.direction = dir;
+		this.rotation = rot;
 	}
 	
 	// Notkun:	a.move();
 	// Fyrir:	ekkert
 	// Eftir: 	this.x
 	public void move() {
-		double speed = 0.01/generation;
-		double dx = speed * Math.cos(Math.toRadians(direction));
-		double dy = speed * Math.sin(Math.toRadians(direction));
+		double dx = this.speed * Math.cos(Math.toRadians(direction));
+		double dy = this.speed * Math.sin(Math.toRadians(direction));
 		
 		this.x = this.x + dx;
 		this.y = this.y + dy;
 		if(this.x < -1 || this.x > 1) this.x *= -1;
 		if(this.y < -1 || this.y > 1) this.y *= -1;
 	}
-	
+
 	public boolean intersects(SpaceObject b) 
 	{
-	
+		return false;
 	}
 	
 	public abstract void draw();
--- asteroid.java
+++ asteroid.java
@@ -1,49 +1,52 @@
 public class asteroid extends SpaceObject {
-	private final int shape, generation;
+	private final int shape, generation, direction;
+	public final double speed;
 	// rotspeed er gráður á hvern ramma (?)
 	private final int rotspd;
-	private final int rotation;
-	private final double direction, speed;
+	//private final int rotation;
 
 	// Notkun:	a1 = new asteroid(generation,degrees);
 	// Fyrir:	0 < gen <= 3
 	// Eftir:	a1 er asteriod af gen kynslóð sem hreyfist í deg átt
-	public asteroid(double x, double y, int gen) {
+	public asteroid(double x, double y, double spd, int dir, int rot,int gen) {
 		generation = gen;
-		direction = (int)(Math.random()*360 +0.5);
-		rotation = (int)(Math.random()*360 +0.5);
-		
-		super.x = x;
-		super.y = y;
-		super.direction = this.direction;
-		super.rotation = this.rotation;
-		
+		direction = dir;
+		speed = 0.1;
 		
 		rotspd = (int)(Math.random()*3 +0.5);
 		shape = (int)(Math.random()*3 +0.5); // ATH! breyta eftir því hve margar teinkingar við fáum
 	}
-
+	
 	public void draw() {
 		double r =  this.generation*0.05;
 		StdDraw.circle(super.x, super.y, r);
 	}
+	/*public void move() {
+		double dx = super.speed * Math.cos(Math.toRadians(super.direction));
+		double dy = super.speed * Math.sin(Math.toRadians(super.direction));
+		
+		super.x = super.x + dx;
+		super.y = super.y + dy;
+		if(super.x < -1 || super.x > 1) this.x *= -1;
+		if(super.y < -1 || super.y > 1) this.y *= -1;
+	}*/
 
 	
 	public static void main(String[] args) {
 	
 		StdDraw.setXscale(-1, 1);
 		StdDraw.setYscale(-1, 1);
-		asteroid a1 = new asteroid(0.5,0.5,1);
-		asteroid a2 = new asteroid(0.0,0.0,2);
-		asteroid a3 = new asteroid(0.0,0.0,3);
+		asteroid a1 = new asteroid(0.5,0.5,0.01,(int)(Math.random()*360 +0.5),(int)(Math.random()*360 +0.5),1);
+	//	asteroid a2 = new asteroid(0.0,0.0,2);
+	//	asteroid a3 = new asteroid(0.0,0.0,3);
 		while(true) {
 			StdDraw.clear();
 			a1.move();
-			a2.move();
-			a3.move();
+	//		a2.move();
+	//		a3.move();
 			a1.draw();
-			a2.draw();
-			a3.draw();
+	//		a2.draw();
+	//		a3.draw();
 			StdDraw.show(20);
 			
 		}
