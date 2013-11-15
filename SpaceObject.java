ba5016d48c19232b5ff824aa730743d19a9ebf53
fortena <arnigeir@gmail.com>
2013-11-15 11:37:32 +0000

    Revert "Veit ekki hvað hefur breyst"
    This reverts commit 6080dfc347323e092fb48c5f9ea7e47281c96215.

--- SpaceObject.java
+++ SpaceObject.java
@@ -1,6 +1,5 @@
 public abstract class SpaceObject {
 	public double x, y;
-	public boolean visible;
 	
 	
 	// Notkun:	a = new SpaceObject(x, y, 0.01, 90, 0);
@@ -8,7 +7,6 @@ public abstract class SpaceObject {
 	public void SpaceObject(double x, double y) {
 		this.x = x;
 		this.y = y;
-		visible = true;
 	}
 	
 	// Notkun:	a.move();
@@ -20,20 +18,13 @@ public abstract class SpaceObject {
 		
 		this.x = this.x + dx;
 		this.y = this.y + dy;
-		if(this.x < -1.2 || this.x > 1.2) this.x *= -1;
-		if(this.y < -1.2 || this.y > 1.2) this.y *= -1;
+		if(this.x < -1 || this.x > 1) this.x *= -1;
+		if(this.y < -1 || this.y > 1) this.y *= -1;
 	}
 
 	public boolean intersects(SpaceObject b) 
 	{
 		return false;
-	}
-	
-	public void hide() {
-		this.visible = false;
-	}
-	public boolean isVisible() {
-		return true;
 	}
 	
 	public abstract void draw();
