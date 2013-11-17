5e797b8b6bdf5372d5947a4427d9d4f667cfa689
GunnarSturla <gunnar@gunnarsturla.com>
2013-11-17 14:09:30 +0000

    asteroid.java inniheldur SimpleVector implementation
    asteroid-s eru núna geymd í SimpleVector, og kallað í þau þaðan

--- asteroid.java
+++ asteroid.java
@@ -36,13 +36,23 @@ public class asteroid extends SpaceObject {
 		int timetilldeath = 100;
 		StdDraw.setXscale(-1, 1);
 		StdDraw.setYscale(-1, 1);
-		asteroid a1 = new asteroid(0.5,0.5,1);
-		asteroid a2 = new asteroid(0.0,0.0,2);
-		asteroid a3 = new asteroid(0.0,0.0,3);
+//		asteroid a1 = new asteroid(0.5,0.5,1);
+//		asteroid a2 = new asteroid(0.0,0.0,2);
+//		asteroid a3 = new asteroid(0.0,0.0,3);
+		
+		SimpleVector a = new SimpleVector();
+			a.add(new asteroid(0.5,0.5,1));
+			a.add(new asteroid(0.0,0.0,2));
+			a.add(new asteroid(0.0,0.0,3));
+			
+	/*	asteroid a[] = new asteroid[3];
+		a[0] = new asteroid(0.5,0.5,1);
+		a[1] = new asteroid(0.0,0.0,2);
+		a[2] = new asteroid(0.0,0.0,3);*/
+		
 		while(true) {
 			StdDraw.clear();
-			for
-			a1.move();
+	/*		a1.move();
 			a2.move();
 			a3.move();
 			a1.draw();
@@ -53,7 +63,28 @@ public class asteroid extends SpaceObject {
 				a3.destroy();
 			} else {
 				timetilldeath--;
+			}*/
+			/*for(int i = 0; i < a.length;i++)
+			{
+				a[i].move();
 			}
+			for(int i = 0; i < a.length;i++)
+			{
+				a[i].draw();
+			}*/
+			asteroid tmp;
+			for(int i = 0; i < a.size() ;i++)
+			{
+				tmp = (asteroid) a.get(i);
+				tmp.move();
+			}
+			for(int i = 0; i < a.size() ;i++)
+			{
+				tmp = (asteroid)a.get(i);
+				tmp.draw();
+			}
+			
+			StdDraw.show(20);
 			
 		}
 		
