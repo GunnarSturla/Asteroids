f366eb2688197203a338c52b59fdce63361c2744
fortena <arnigeir@gmail.com>
2013-11-17 11:40:24 +0000

    jabby

---
+++ Game.java
@@ -0,0 +1,20 @@
+public class Game
+{
+	public static void main(String[] args)
+	{
+	int N = 20;
+	Ship s = new Ship (0.0, 0.0, 0.02, 0.02, 3, 90, true);
+	Astroid[] a = new astroid[N];
+	for (int i = 0; i < N; i++)
+	{
+		a[i] = Math.random(), Math.random(), 1;
+
+	}
+	while (a.isAlive)
+	{
+		StdDraw.a;
+		StdDraw.s;
+		StdDraw.show(20);
+	}
+	}
+}
